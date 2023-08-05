9fd2a99e37fa0131796c0b3a53a9e027206f5adc
Amey <ameykurade60@gmail.com>
2023-08-05 22:15:25 +0530

    Partial-for-loop

---
+++ partial-for-loop/src/partial/Partial.java
@@ -0,0 +1,17 @@
+//Normal For Partial Loop
+package partial;
+import java.util.*;
+
+public class Partial{
+    public static void main(String[] args) {
+        float p, r, si;
+        int n, count;
+        Scanner sc;
+
+        sc = new Scanner(System.in);
+        for (count = 1; count <= 10; count++)
+        {
+            System.out.println(count);
+        }
+    }
+}
