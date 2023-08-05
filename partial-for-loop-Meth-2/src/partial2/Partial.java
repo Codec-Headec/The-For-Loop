6c9b8acf105be4aa5410bdd240aae9a3da2c3926
Amey <ameykurade60@gmail.com>
2023-08-05 22:20:56 +0530

    Method 2

--- partial-for-loop-Meth-2/src/partial2/Partial.java
+++ partial-for-loop-Meth-2/src/partial2/Partial.java
@@ -1,15 +1,15 @@
 //Method 1: Normal For Partial Loop
-package partial;
+package partial2;
 import java.util.*;
 
 public class Partial{
     public static void main(String[] args) {
         float p, r, si;
-        int n, count;
+        int n, count = 1;
         Scanner sc;
 
         sc = new Scanner(System.in);
-        for (count = 1; count <= 10; count++)
+        for ( ; count <= 10; count++)
         {
             System.out.println(count);
         }
